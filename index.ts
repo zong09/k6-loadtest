ad9b6ce3566ea674c7ad08f2ebaf780c7eefb4c6
Zong <zong09@hotmail.com>
2023-10-25 15:59:50 +0700

    init project

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "1m30s", target: 10 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get("http://localhost:3000/");
+  check(res, { "status was 200": (r) => r.status == 200 });
+//   sleep(1);
+}
