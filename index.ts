70686b8a8445d8f92d5ba4d9dbf6f06792cc495b
Zong <zong09@hotmail.com>
2023-10-25 16:13:53 +0700

    update test

--- index.ts
+++ index.ts
@@ -1,16 +1,24 @@
-import { check, sleep } from "k6";
-import http from "k6/http";
+import { check, sleep } from 'k6';
+import http from 'k6/http';
 
+// Test configuration
 export const options = {
+  thresholds: {
+    // Assert that 99% of requests finish within 3000ms.
+    http_req_duration: ['p(99) < 3000'],
+  },
+  // Ramp the number of virtual users up and down
   stages: [
-    { duration: "30s", target: 20 },
-    { duration: "1m30s", target: 10 },
-    { duration: "20s", target: 0 },
+    { duration: '30s', target: 15 },
+    { duration: '1m', target: 20 },
+    { duration: '20s', target: 0 },
   ],
 };
 
+// Simulated user behavior
 export default function () {
-  const res = http.get("http://localhost:3000/");
-  check(res, { "status was 200": (r) => r.status == 200 });
-//   sleep(1);
+  const res = http.get('http://localhost:3000/');
+  // Validate response status
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
 }
