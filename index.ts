9043ae7c23dc7e401f6571d8afbafbed1439b186
Zong <zong09@hotmail.com>
2024-01-18 12:28:06 +0700

    update test case

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { check, sleep } from 'k6';
 import http from 'k6/http';
 
 // Test configuration
@@ -10,15 +9,32 @@ export const options = {
   // Ramp the number of virtual users up and down
   stages: [
     { duration: '30s', target: 15 },
-    { duration: '1m', target: 20 },
+    { duration: '1m', target: 50 },
+    { duration: '20s', target: 200 },
     { duration: '20s', target: 0 },
   ],
 };
 
 // Simulated user behavior
+// export default function () {
+//   const res = http.get('http://localhost:8080/ping');
+//   // Validate response status
+//   check(res, { 'status was 200': (r) => r.status == 200 });
+//   sleep(1);
+// }
+
 export default function () {
-  const res = http.get('http://localhost:3000/');
-  // Validate response status
-  check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
+  const url = 'http://localhost:3000/api/v1/auth/login';
+  const payload = JSON.stringify({
+    username: 'john',
+    password: 'changeme',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
 }
